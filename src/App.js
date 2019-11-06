ae5247609f5328761ad2d8dfb1b58125c4213f55
grantkee <grant.d.kee@gmail.com>
2019-11-05 18:29:00 -0600

    trying different method of creating divs, but nothing is mapping because my json object is empty.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class User extends Component {
   constructor(props) {
     super(props);
-    this.state = { isVerified: false, users: []}
+    this.state = { users: [], isVerified: false, viewDetails: false}
   }
 
   componentDidMount() {
@@ -14,18 +14,28 @@ class User extends Component {
 
   fetchData() {
     fetch ('https://randomuser.me/api?results=50')
-    .then(response => response.json())
-    .then(parsedJSON => parsedJSON.results.map(contact => (
-      {}
-    )))
-    .then(users => this.setState(
+    .then(response => response.results.json())
+    .then(contact => this.setState(
       {
-        users,
+        users: contact,
         isVerified: false,
         viewDetails: false
       }
     ))
     .catch(error => console.log("parsing failed", error))
+  }
+
+  sortContacts = () => {
+    const newContact = this.state.users.map((contact) => {
+      return (
+        <div className="basic-userInfo">
+          {contact.name.first} {contact.name.last}
+          <img src={contact.picture.thumbnail} alt={contact.login.username} className="user-thumbnail-basic"></img>
+          <button className="details-button" onClick={this.viewUserDetails}>Details</button>
+        </div>
+      )
+    })
+    return newContact
   }
 
   isVerified = (e) => {
@@ -39,41 +49,51 @@ class User extends Component {
   }
 
   render() { 
-    const {isVerified, users, showDetails} = this.state;
-    return ( 
+    return (
       <div>
         <header>
-          <h1>Your Contacts</h1><br></br>
+          <h1>Your Contacts</h1><br/>
           Verify Friends for Offline Use
-          </header>
-          <div className="container">
-            {users.map(userInfo => {
-              const {id, username, firstname, lastname, pic, phone, city, lat, long} = userInfo;
-              return (
-                <div key={userInfo.id} title={userInfo.username} className="user-profile">
-                  {!this.state.showDetails ? (
-                  <div className="basic-userInfo">
-                    {firstname} {lastname}
-                    <img src={pic} alt={username} className="user-thumbnail-basic"></img>
-                    <button className="details-button" onClick={this.viewUserDetails(userInfo.id)}>Details</button>
-                  </div>
-                  ) : (
-                  <div className="detailed-userInfo">
-                    {firstname} {lastname}
-                    <img src={pic} alt={username} className="user-thumbnail-detailed"></img>
-                    <ul>
-                      <li>{username}</li>
-                      <li>{phone}</li>
-                      <li>{city}</li>
-                    </ul>
-                  </div>
-                  )}
-                </div>
-              )
-            })}
-          </div>
+        </header>
+           {this.sortContacts()}
       </div>
-     );
+    )
+
+    // const {isVerified, users, showDetails} = this.state;
+    // return (
+    //   <div>
+    //     <header>
+    //       <h1>Your Contacts</h1><br></br>
+    //       Verify Friends for Offline Use
+    //       </header>
+    //       <div className="container">
+    //         {users.map(userInfo => {
+    //           const {id, username, firstname, lastname, pic, phone, city, lat, long} = userInfo;
+    //           return (
+    //             <div key={userInfo.id} title={userInfo.username} className="user-profile">
+    //               {!this.state.showDetails ? (
+    //               <div className="basic-userInfo">
+    //                 {firstname} {lastname}
+    //                 <img src={pic} alt={username} className="user-thumbnail-basic"></img>
+    //                 <button className="details-button" onClick={this.viewUserDetails(userInfo.id)}>Details</button>
+    //               </div>
+    //               ) : (
+    //               <div className="detailed-userInfo">
+    //                 {firstname} {lastname}
+    //                 <img src={pic} alt={username} className="user-thumbnail-detailed"></img>
+    //                 <ul>
+    //                   <li>{username}</li>
+    //                   <li>{phone}</li>
+    //                   <li>{city}</li>
+    //                 </ul>
+    //               </div>
+    //               )}
+    //             </div>
+    //           )
+    //         })}
+    //       </div>
+    //   </div>
+    //  );
   }
 }
  
